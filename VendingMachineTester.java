d755e2986c9f75c85002aa46426ec386aca2087a
William Gusmanov <Williamgusmanov1@gmail.com>
2019-10-09 10:01:53 -0700

    updated tester
    updated tester

--- VendingMachineTester.java
+++ VendingMachineTester.java
@@ -1,11 +1,19 @@
 package designingclasses;
-
+import java.util.ArrayList; 
 public class VendingMachineTester {
 
 	public static void main(String[] args) {
-		vendingMachine x = new vendingMachine(); 
-		x.insertCoin("penny");
-		x.insertCoin("bitcoin");
+		ArrayList<Item> newSupply = new ArrayList<Item>();
+		//item: price, number, name
+		Item Lays = new Item(0.75f, 4, "Lays");
+		Item Cola = new Item(1.25f, 2, "Cola");
+		Item Snickers = new Item(0.5f,6, "Snickers");
+		newSupply.add(Lays);
+		newSupply.add(Cola);
+		newSupply.add(Snickers); 
+		vendingMachine x = new vendingMachine(newSupply); 
+		x.displayItems();
+		x.customerAccess();	
 	}
 
 }
